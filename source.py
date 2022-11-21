b34ee1406acf6289704356d1c2d5bf38c1d74ba8
cakuakz <rafiirvansyah88@gmail.com>
2022-11-22 01:02:46 +0700

    fact table source

---
+++ source.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import os
+import math
+
+# delete file if exist
+if os.path.exists('csv/Fact_Source.csv'):
+    os.remove('csv/Fact_Source.csv')
+
+
+# Read data
+source=pd.read_csv('csv/Dim_Source.csv')
+participants=pd.read_csv('csv/Dim_Participant.csv')
+
+# Merge data
+sales=pd.merge(participants,source,on=['id'],how='inner')
+
+# create new file named fact_sales.csv
+sales.to_csv('csv/Fact_Source.csv',index=False)
